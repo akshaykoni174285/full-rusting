b925f5e73df2d58dd6dc4fe8091057e5061e54e1
akshaykoni174285 <akshaykoni174285@gmail.com>
2022-10-01 11:02:52 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod print;
+// you can import a file or module like this 
+
+// use hello::*;
+fn main() {
+    // println!("Hello, world!");
+    // hello::run(); and then you have to use the module name and then the function name to use it 
+    print::run();
+    // you are suppose to use the module name 
+
+}
---
+++ src/print.rs
@@ -0,0 +1,4 @@
+pub fn run(){
+    println!("hello world from the hello.rs file");
+    
+}
